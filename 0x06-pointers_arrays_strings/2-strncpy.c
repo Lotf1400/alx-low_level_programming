113fb4d6139dd2952639eb3062b8e927b04026eb
Hicham <www.hicham1400@gmail.com>
2023-03-31 17:32:45 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#inlude "main.h"
+/**
+ * _strncpy: copy a string
+ * @dest: input value
+ * @src: value to input
+ * @n: value to input
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
