8798f6ed23d072eadd5d6e0c69936c5b26ea1f61
Hicham <www.hicham1400@gmail.com>
2023-04-03 19:45:53 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - locates a string
+ * @needle: the substring
+ * @haystack: the string where to find the first occurence
+ * Return: 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	 int i;
+
+        while (*s)
+        {
+                for (i = 0; accept[i]; i++)
+                {
+
+                if (*s == accept[i])
+                        return (s);
+                }
+                s++;
+        }
+        return ('\0');
+}
