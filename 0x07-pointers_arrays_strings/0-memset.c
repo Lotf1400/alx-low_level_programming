8d2495c72787467131b5b4532dfad646bfa09a6c
Hicham <www.hicham1400@gmail.com>
2023-04-03 18:05:31 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,8 +6,7 @@
  * @n: the byte to be changed
  * Return: change the array with a new value
  */
-
-int *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
