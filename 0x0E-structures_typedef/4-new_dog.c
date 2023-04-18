02f688d2f589843dc27e9d94632aafc5053453c2
Hicham <www.hicham1400@gmail.com>
2023-04-17 18:07:11 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -40,7 +40,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] != '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
@@ -55,21 +55,22 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int a, b;
+	int len1, len2;
 	
-	a = _strlen(name);
-	b = _strlen(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 	
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (a + 1));
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (b + 1));
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	
 	if (dog->owner == NULL)
 	{
 		free(dog);
