b6341f85d6ce841a2e7176bfd7eed840533db72c
Hicham <www.hicham1400@gmail.com>
2023-04-28 18:59:24 -0700

    free_listint_safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * free_listint_safe - it frees a listint_t
+ * list
+ * @h: a ptr
+ * Return: the size of the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t delf = 0;
+	int i;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		i = *h - (*h)->next;
+		if (i > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			delf++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			delf++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (delf);
+}
